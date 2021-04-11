f850dbfcb85a48cb4230a516d3856b390e0e5fbc
Evgeniya3010 <susskaya86@gmail.com>
2021-04-12 02:45:17 +0300

    practice

--- src/redux/counter/counter-actions.js
+++ src/redux/counter/counter-actions.js
@@ -1,13 +1,18 @@
+import { createAction } from '@reduxjs/toolkit';
 import actionTypes from './counter-types';
 
-const increment = value => ({
-  type: actionTypes.INCREMENT,
-  payload: value,
-});
+// const increment = value => ({
+//   type: actionTypes.INCREMENT,
+//   payload: value,
+// });
 
-const decrement = value => ({
-  type: actionTypes.DECREMENT,
-  payload: value,
-});
+// const decrement = value => ({
+//   type: actionTypes.DECREMENT,
+//   payload: value,
+// });
+
+// toolkit
+const increment = createAction(actionTypes.INCREMENT);
+const decrement = createAction(actionTypes.DECREMENT);
 
 export default { increment, decrement };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,81 +1,28 @@
-import { createStore, combineReducers } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-
+// import { combineReducers } from 'redux';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 import counterReducer from './counter/counter-reducer';
-
-// для работы redux девтулзов
-// const REDUX_DEVTOOLS =
-//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-
-// начальный стейт для всех компонетов
-// const initialState = {
-//   counter: {
-//     value: 10,
-//     step: 7,
-//   },
-// };
-
-// const reducer = (state = initialState, action) => {
-//   switch (action.type) {
-// кейсы для каждого типа action
-// case 'counter/Increment':
-//   return {
-//   payload - значение из actions
-//         ...state,
-//         counter: {
-//           ...state.counter,
-//           value: state.counter.value + action.payload,
-//         },
-//       };
-
-//     case 'counter/Decrement':
-//       return {
-//         ...state,
-//         counter: {
-//           ...state.counter,
-//           value: state.counter.value - action.payload,
-//         },
-//       };
-
-//     default:
-//       return state;
-//   }
-// };
-
-// начальный стейт для countera
-// const counterInitialState = {
-//   value: 10,
-//   step: 7,
-// };
-
-// // редюсер для value
-// const valueReducer = (state = 10, { type, payload }) => {
-//   switch (type) {
-//     case 'counter/Increment':
-//       return state + payload;
-
-//     case 'counter/Decrement':
-//       return state - payload;
-
-//     default:
-//       return state;
-//   }
-// };
-
-// // редюсер для step
-// const stepReducer = (state = 7, action) => state;
-
-// редюсер для countera
-// const counterReducer = combineReducers({
-//   value: valueReducer,
-//   step: stepReducer,
-// });
+// import { composeWithDevTools } from 'redux-devtools-extension';
 
 // глобальній общий редюсер обїединяет все редюсері
-const rootReducer = combineReducers({
-  counter: counterReducer,
-});
+// const rootReducer = combineReducers({
+//   counter: counterReducer,
+// });
 
-const store = createStore(rootReducer, composeWithDevTools());
+// логируем action в консоли как в редакс-тулзах
+const middleware = [...getDefaultMiddleware(), logger];
+
+// const store = createStore(rootReducer, composeWithDevTools());
+
+console.log(process.env.NODE_ENV);
+
+// вместо combinedreducers кидаем сразу в стор объект редюсер
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+  middleware,
+  devTools: process.env.NODE_ENV === 'development',
+});
 
 export default store;
